50b8c949f85526e933cd6d6d84590636991ac813
prabhakar kumar <70058887+prabhakarzha@users.noreply.github.com>
2021-02-06 15:09:58 +0530

    first commit

---
+++ variable.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Table Variable</title>
+</head>
+<body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+             
+  <table class="table table-striped"
+  
+  
+  
+<?php
+$a =10;
+$b =20;
+$c= $a+$b;
+
+
+$d =20;
+$e=30;
+$f=$d+$e;
+?>
+>
+    <thead>
+      <tr>
+        <th>Value of A</th>
+        <th>Value of B</th>
+        <th>Sum of  A & B</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td><?php echo $a ?></td>
+        <td><?php echo $b ?></td>
+        <td> <?php echo $c ?></td>
+      </tr>
+      <tr>
+        <td><?php echo $d?></td>
+        <td><?php echo $e?></td>
+        <td><?php echo $f?></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+
+</body>
+</html>
+
+
+
+
+    
+</body>
+</html>
+
+
+
+
+
